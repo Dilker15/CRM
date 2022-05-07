43a71f02919a74060406bb0aed74bbfd92dccf57
Carl2170 <vargascarlos2170@gmail.com>
2022-05-07 13:38:07 -0400

    Primer commit

---
+++ app/Http/Controllers/Cliente/ClienteController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Cliente;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Cliente\Cliente;
+class ClienteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   $clientes = Cliente::get();
+        return view('clientes.index',compact('clientes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clientes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $inputs = $request->all();
+        Cliente::create($inputs);
+        return redirect()->route('clientes.index')->with('success','Cliente creado con Exito');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cliente $cliente)
+    {
+        return view('clientes.edit',compact('cliente'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,Cliente $cliente)
+    {
+            $inputs = $request->all();
+            $cliente->update($inputs);
+            return redirect()->route('clientes.index')->with('success','Cliente Editado correctamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $inputs = $request->all();
+        $id = $inputs['cliente_id'];
+        $cliente = Cliente::find($id);
+        $cliente->update([
+            'estado' => 2,
+        ]);
+        return redirect()->route('clientes.index')->with('danger','Cliente Eliminado Existosamente');
+    }
+}
