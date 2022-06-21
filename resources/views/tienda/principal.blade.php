5cc939519987d9f8d9abbab54a2b74126fbff4cb
CVargas2170 <cvargas2170@gmail.com>
2022-06-21 12:06:35 -0400

    otro commit1

--- resources/views/tienda/principal.blade.php
+++ resources/views/tienda/principal.blade.php
@@ -52,9 +52,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oleo+Script+Swash+Caps&display=swap" rel="stylesheet">
     <!--*********************************************************************************************************************************************************************-->
-    <link rel="stylesheet" href="{{secure_asset('css/s1.css')}}">
+  
     <link rel="stylesheet" href="{{secure_asset('css/oficial.css')}}">
-    <link rel="stylesheet" href="css/oficial.css">
+ 
   </head>
 
   <body>
