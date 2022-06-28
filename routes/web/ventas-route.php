e21f91cdc2154574070681e4161c80fef4828231
CVargas2170 <cvargas2170@gmail.com>
2022-06-28 04:50:21 -0400

    2do arreglo a ventas

--- routes/web/ventas-route.php
+++ routes/web/ventas-route.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VentaController;
+use App\Http\Controllers\Venta\VentaController;
 use App\Http\Controllers\Cliente\ClienteController;
 
 
